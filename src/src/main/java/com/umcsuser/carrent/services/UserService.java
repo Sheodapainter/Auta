0ba9a39756e1f102e71088bc60bd7c7b1dfff56f
Sheodapainter <mgarbiec9@gmail.com>
2026-06-09 18:07:36 +0200

    Add files via upload

---
+++ src/src/main/java/com/umcsuser/carrent/services/UserService.java
@@ -0,0 +1,55 @@
+package com.umcsuser.carrent.services;
+
+import com.umcsuser.carrent.models.Rental;
+import com.umcsuser.carrent.models.User;
+import com.umcsuser.carrent.repositories.RentalRepository;
+import com.umcsuser.carrent.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserService implements UserServiceInterface{
+    private final UserRepository userRepository;
+    private final RentalRepository rentalRepository;
+
+    public UserService(UserRepository userRepository, RentalRepository rentalRepository) {
+        this.userRepository = userRepository;
+        this.rentalRepository = rentalRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public User findById(String id) {
+        Optional<User> u = userRepository.findById(id);
+        return u.orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(String id, String loggedUserId) {
+        if(id==null) {
+            throw new IllegalArgumentException("Id nie może być null!");
+        }
+        if(Objects.equals(id, loggedUserId)) {
+            throw new IllegalArgumentException("Nie można usunąć samego siebie!");
+        }
+        Optional<Rental> r = rentalRepository.findAll().stream()
+                .filter(rental -> rental.getUserId().equals(id))
+                .filter(rental -> rental.getReturnDateTime()==null).findFirst();
+        if(r.isPresent()) {
+            throw new IllegalArgumentException("Użytkownik posiada wypożyczony pojazd!");
+        }
+        userRepository.deleteById(id);
+    }
+}
