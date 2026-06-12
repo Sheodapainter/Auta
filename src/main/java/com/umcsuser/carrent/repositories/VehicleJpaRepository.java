40a6517e720937e857aa02255abe6e564d987aef
Sheodapainter <mgarbiec9@gmail.com>
2026-06-12 11:24:45 +0200

    Delete src directory

--- src/main/java/com/umcsuser/carrent/repositories/VehicleJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.umcsuser.carrent.repositories;
-
-import com.umcsuser.carrent.models.Vehicle;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Profile("jpa")
-public interface VehicleJpaRepository extends JpaRepository<Vehicle, String> {
-}
